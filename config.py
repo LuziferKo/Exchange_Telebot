894ae2f7f56e346915817f1ad7918d5febeeeb95
LuziferKo <139195156+LuziferKo@users.noreply.github.com>
2023-10-18 17:19:02 +0900

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = '6614903822:AAH3IrBKXJ8SjvGxqrpzguhLY6LT9oIllFY'
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'рубль' : 'RUB',
+    'евро' : 'EUR',
+}
